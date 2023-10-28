947051205b6dfa40f8db19661d7430f38edb7c9b
Manuelleon123 <manuel.j.leon.c2@gmail.com>
2023-10-28 17:46:43 -0400

    calculator java

---
+++ main.js
@@ -0,0 +1,24 @@
+var operandoa;
+var operandob;
+var operacion;
+
+function init(){
+    //variables
+    var resultado = document.getElementById('resultado');
+    var reset = document.getElementById('reset');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('1');
+    var dos = document.getElementById('2');
+    var tres = document.getElementById('3');
+    var cuatro = document.getElementById('4');
+    var cinco = document.getElementById('5');
+    var seis = document.getElementById('6');
+    var siete = document.getElementById('7');
+    var ocho = document.getElementById('8');
+    var nueve = document.getElementById('9');
+    var cero = document.getElementById('0');
+  }
